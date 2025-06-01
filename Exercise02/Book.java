1953712f76412eb857fe4210997caff7bad9849a
Dung20012 <hantho1107@gmail.com>
2025-06-01 20:56:47 +0700

    Add files via upload

---
+++ Exercise02/Book.java
@@ -0,0 +1,41 @@
+package Exercise02;
+
+public class Book {
+//    Xây dựng lớp Book và tính bao đóng
+//    Thuộc tính
+     private String title;
+     private String author;
+     private double price;
+
+//     Phương thức getter cho title
+    public String getTitle(){
+        return title;
+    }
+//    Phương thức setter cho title
+    public void setTitle(String title){
+        this.title = title;
+    }
+
+//    Phương thức getter cho author
+    public String getAuthor(){
+        return author;
+    }
+//    Phương thức setter cho author
+    public void setAuthor(String author){
+        this.author = author;
+    }
+
+//    Phương thức getter cho price
+    public double getPrice(){
+        return price;
+    }
+
+//    Phương thức setter cho price và ktra tính hợp lệ
+    public void setPrice(double price){
+        if (price < 0){
+            System.out.println("Giá không được âm");
+        }else {
+            this.price = price;
+        }
+    }
+}
